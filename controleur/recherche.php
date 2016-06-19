0ace9f8083f81ea9d4000be21803cc6ca028cea7
Guillaume <gjauffret21@gmail.com>
2016-06-19 19:05:13 +0200

    images

--- controleur/recherche.php
+++ controleur/recherche.php
@@ -3,14 +3,22 @@
 include_once("modele/membre.php");
 include_once("modele/events.php");
 include_once("modele/connexion_bdd.php");
+include("modele/co_in.php");
 
-$categories = get_categories();
-$tags = get_tags();
-$events = get_events();
+if(isset($_SESSION["id_user"])){
+	$categories = get_categories();
+	$tags = get_tags();
+	$events = get_events();
 
-if(isset($_POST["nom"])){
+	if(isset($_POST["nom"])){
 	echo "Recherche ok".$_POST["nom"];
-}elseif (isset($_POST["stags"])) {
-$events = get_events_by_tags($_POST["tags"]);
+	}elseif (isset($_POST["stags"])) {
+	$events = get_events_by_tags($_POST["tags"]);
+	}
+	include_once("vue/recherche.html");
+}else{
+	$message = "Connectez-vous pour accéder à cette page";
+	include_once("vue/connexion.php");
 }
-include_once("vue/recherche.html");
+
+
