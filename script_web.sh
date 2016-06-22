3fe9ffe9616b75d4aac5eac7660a81152c7ed8f1
Guillaume <gjauffret21@gmail.com>
2016-06-22 18:51:46 +0200

    scripts

--- script_web.sh
+++ script_web.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 # 1er param : 1 => ajouter ; 2 => supprimer
-# 2e param : nom 
+# 2e param : organisateur
 # 3e param :  1 => importe son propre site ; 2 => utilise wordpress
 # 4e param : mot de passe de la boîte mail
+# 5e param : nom évènement 
 
 source /var/www/eventizi.itinet.fr/scripts/source.sh
 
-if [ $# != 4 ]
+if [ $# != 5 ]
 then
 sudo bash -c "echo 'missing arguments'"
 exit
 fi
 
-sudo script_vhosts.sh $1 $2
-sudo script_fqdn.sh $1 $2
-#sudo script_bdd.sh $1 $2
-sudo script_mail.sh $1 $2 $4
-if [ $3 = 1 ] 
-then
-	sudo script_mss.sh $1 $2
+sudo ./script_vhosts.sh $1 $2 $5
+sudo ./script_fqdn.sh $1 $5
+#sudo ./script_bdd.sh $1 $2
+sudo ./script_mail.sh $1 $5 $4
 
-else
-	if [ $1 = 1 ]
+
+	if [ $1 = 1 && $3 = 2 ]
 	then
-		sudo cp -a /var/www/eventizi.itinet.fr/wordpress/wordpress/* /var/www/$2
-		sed -i "s/identifiant/$2/g" /var/www/$2/wp-config.php
+		sudo cp -a /var/www/eventizi.itinet.fr/wordpress/wordpress/* $www$2/$5
+		sed -i "s/identifiant_user/$2/g" $www$2/$5/wp-config.php
+		sed -i "s/identifiant_bdd/$5/g" $www$2/$5/wp-config.php
 		sed -i "s/mdp/$2/g" /var/www/$2/wp-config.php
 
 	else
