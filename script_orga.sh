778673d468a5e625ef890f2dc63d3d687a272426
tjchris34 <thiburce@intechinfo.fr>
2016-06-22 14:26:45 +0200

    modif script orga

--- script_orga.sh
+++ script_orga.sh
@@ -4,20 +4,29 @@
 # $2 Nom de l'évenement
 source /var/www/eventizi.itinet.fr/Integration/source.sh
 case $1 in 
-{
+
 		1)
 				var=`cut -d '}' -f 1 /etc/prosody/prosody.cfg.lua |grep admins`
                 a=$(echo $var | sed 's/[ \t]*$//');
                 # echo $a
-                var2=",\"$2@$dname\""
-                result="$a$var2"
+                b=$(echo $a | grep $2@$dname);
+                if [ -z $a ] then 
+
+                        var2=",\"$2@$dname\""
+                        result="$a$var2"
                 # echo $result
-                sudo sed -i.bak 's/^'"$a"'/'"$result"'/' /etc/prosody/prosody.cfg.lua
+                        sudo sed -i.bak 's/^'"$a"'/'"$result"'/' /etc/prosody/prosody.cfg.lua
+                else exit 1
+                fi       
         ;;
 
-        2) 
-				sudo sed -i 's/'"\,\"$2@$dname\""'//' /etc/prosody/prosody.cfg.lua
-}                 
-                 
-           
+        2)
+                if [ -n $2 ] then 
+				
+                                sudo sed -i 's/'"\,\"$2@$dname\""'//' /etc/prosody/prosody.cfg.lua
+                else exit 1
+               fi  
+        ;;
+
+esac           
 
