a9fcc085df91869416503d51eb101f80876797f2
kasuke5 <ratchet_furtif@yahoo.fr>
2016-06-22 12:48:54 +0200

    maj de theme

--- controleur/recherche.php
+++ controleur/recherche.php
@@ -1,7 +1,20 @@
 <?php
 
-
-	include_once("vue/recherche.php");
-
-
-
+include_once("modele/membre.php");
+include_once("modele/events.php");
+include_once("modele/connexion_bdd.php");
+include("modele/co_in.php");
+if(isset($_SESSION["id_user"])){
+	$categories = get_categories();
+	$tags = get_tags();
+	$events = get_events();
+	if(isset($_POST["nom"])){
+	echo "Recherche ok".$_POST["nom"];
+	}elseif (isset($_POST["stags"])) {
+	$events = get_events_by_tags($_POST["tags"]);
+	}
+	include_once("vue/recherche.html");
+}else{
+	$message = "Connectez-vous pour accéder à cette page";
+	include_once("controleur/connexion.php");
+}
