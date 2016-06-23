17a286575f7e696146f8896c4e9be9abf5e62323
tjchris34 <thiburce@intechinfo.fr>
2016-06-23 10:36:57 +0200

    modif action controleur

--- controleur/actions.php
+++ controleur/actions.php
@@ -8,16 +8,16 @@
 	remove_event($_POST["id"],$_SESSION["id_user"]);
 	if($event["site"] ==1){
 		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 2 ".$event["event_title"]." ".$_SESSION["login_user"];
-		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"]." 1";
+		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"];
 		exec($commande_bdd);
 	}elseif ($event["site"] ==2) {
 		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 1 ".$event["event_title"]." ".$_SESSION["login_user"];
 	}elseif ($event["site"] ==3) {
 		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 1 ".$event["event_title"]." ".$_SESSION["login_user"];
-		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"]." 1";
+		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"];
 		exec($commande_bdd);
 	}
 	exec($commande_web);
 }
 
-include "controleur/tableau_bord.php";
+include("controleur/tableau_bord.php");
