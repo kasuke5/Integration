aab0eb457cb7d2f9596b4e9df38e4b5f49e1411c
tjchris34 <thiburce@intechinfo.fr>
2016-06-24 21:13:23 +0200

    modif page connexion

--- vue/connexion.php
+++ vue/connexion.php
@@ -1,9 +1,10 @@
 
 <head>
- <link rel="stylesheet" href="css/reset.css">
+ <link rel="stylesheet" href="vue/css/reset.css">
 
     <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
 <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+ <link rel="stylesheet" href="vue/css/style.css">
 </head>
   <body>
 <div class="container">
@@ -55,7 +56,7 @@
 </div>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-        <script src="js/index.js"></script>  
+        <script src="vue/js/index.js"></script>  
   
   </body>
 
