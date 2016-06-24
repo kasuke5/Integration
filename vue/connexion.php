34afb273fc1d3f91bac1af9bb50294bd8468b40c
tjchris34 <thiburce@intechinfo.fr>
2016-06-24 21:30:06 +0200

    modif page connexion

--- vue/connexion.php
+++ vue/connexion.php
@@ -18,7 +18,7 @@ include('header.php');
         <div class="bar"></div>
       </div>
       <div class="button-container">
-        <button><span>Connexion</span></button>
+        <button type="submit" class="btn btn-default" name="action"><span>Connexion</span></button>
       </div>
     </form>
   </div>
@@ -57,6 +57,38 @@ include('header.php');
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
         <script src="vue/js/index.js"></script>  
+        
+<script>
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+</script>
+
   
   </body>
 
