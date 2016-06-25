416030e91475be51e544124ed89ec5e41d698745
tjchris34 <thiburce@intechinfo.fr>
2016-06-25 17:41:10 +0200

    modif page connexion

--- vue/connexion.php
+++ vue/connexion.php
@@ -9,48 +9,11 @@ include('header.php');
   <h2 class="login-header">Log in</h2>
 
   <form class="login-container">
-    <p><input type="text" name="login" required class="form-control" id="login" placeholder="Pseudo"></p>
-    <p><input type="password" name="pass" required class="form-control" id="pass" placeholder="Mot de passe"></p>
-    <p><input type="submit" class="btn btn-default" name="action" value="Connexion"></p>
+    <p><input type="text" name="login" id="login" placeholder="Pseudo"></p>
+    <p><input type="password" name="pass" id="pass" placeholder="Mot de passe"></p>
+    <p><input type="submit" name="action" value="Connexion"></p>
   </form>
 </div>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-    
-    
-    
-
-<script>
-$(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip(); 
-  
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-})
-</script>
-
-  
   </body>
 
