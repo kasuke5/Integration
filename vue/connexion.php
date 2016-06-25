eb9c1e4b6bfdb08de97e02501cf9a64f7c8ac4e8
tjchris34 <thiburce@intechinfo.fr>
2016-06-25 17:53:23 +0200

    modif page connexion

--- vue/connexion.php
+++ vue/connexion.php
@@ -8,7 +8,7 @@ include('header.php');
   
   <h2 class="login-header">Connexion</h2>
 
-  <form class="form-horizontal">
+  <form class="form-horizontal" role="form" method="post">
     <p><input type="text" name="login" id="login" placeholder="Pseudo"></p>
     <p><input type="password" name="pass" id="pass" placeholder="Mot de passe"></p>
     <p><input type="submit" name="action" value="Connexion"></p>
