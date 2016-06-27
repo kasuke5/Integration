480f2b638511f272cac77a2feef399d09a92a03c
tjchris34 <thiburce@intechinfo.fr>
2016-06-27 23:45:59 +0200

    modif page ajout event

--- vue/header.php
+++ vue/header.php
@@ -147,7 +147,17 @@ jQuery(".window").hide();
 <!--La box-->
 <div id="dialog" class="window"/>
 <center> 
- 
+ <div class="login">
+  <div class="login-triangle"></div>
+  
+  <h2 class="login-header">Connexion</h2>
+
+  <form class="form-horizontal" role="form" method="post">
+    <p><input type="text" name="login" id="login" placeholder="Pseudo"></p>
+    <p><input type="password" name="pass" id="pass" placeholder="Mot de passe"></p>
+    <p><input type="submit" name="action" value="Connexion"></p>
+  </form>
+</div>
 <input type="button" class="close" value="Quitter" />
 </center>
 </div>
