a6dee0c462376bbeec5bd0701a6f565f7596cda8
tjchris34 <thiburce@intechinfo.fr>
2016-06-27 23:41:50 +0200

    modif page ajout event

--- vue/connexion.php
+++ vue/connexion.php
@@ -2,15 +2,8 @@
 include('header.php');
 ?>
   <body>
-  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-  <div class="modal-content">
-  <div class="modal-header">
-  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-  <h4 class="modal-title" id="myModalLabel">Connexion</h4>
-  </div>
-  <div class="modal-body">
-  <div class="login">
+
+<div class="login">
   <div class="login-triangle"></div>
   
   <h2 class="login-header">Connexion</h2>
@@ -20,11 +13,6 @@ include('header.php');
     <p><input type="password" name="pass" id="pass" placeholder="Mot de passe"></p>
     <p><input type="submit" name="action" value="Connexion"></p>
   </form>
-  </div>
-  </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
-
 </div>
 <?php
 include 'footer.php';
