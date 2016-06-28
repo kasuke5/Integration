74a1e964091ca79eeb782b7f534d0d2a787aceca
tjchris34 <thiburce@intechinfo.fr>
2016-06-28 14:41:15 +0200

    modif page ajout event

--- vue/header.php
+++ vue/header.php
@@ -139,7 +139,36 @@ jQuery(".window").hide();
               echo'<li><a href="/recherche">Recherche</a></li><li><a href="/tableau_bord">Mes évènements</a></li><li><a href="/deconnexion">Se déconnecter</a></li><li><a href="http://mail.eventizi.itinet.fr" target="_blank">Boîte Mail</a></li> ';
             }else{
               #echo'<li><a href="vue/modal.php">Connexion</a></li><li><a href="/inscription">Inscription</a></li></div>';
-              echo'<li><a href="#modal-2" data-toggle="modal">connexion</a></li>';
+              echo'<li><a href="#modal-2" data-toggle="modal">connexion</a></li>
+              <div class="modal" id="modal-2">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="login">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      <div class="login-triangle"></div>
+                      <h2 class="login-header">Connexion</h2>
+                    </div>
+                    <div class="modal-body">
+                      <form class="form-horizontal" role="form" method="post">
+                      <div class="row">
+                                                <div class="col-md-6">
+                            <input type="text" name="login" id="login" placeholder="Pseudo">
+                            <br />
+                            <input type="password" name="pass" id="pass" placeholder="Mot de passe">
+                            <br />
+                                                </div>
+                                            </div>
+                                                <div class="modal-footer">
+                            <button type="button" data-dismiss="modal" class="btn btn-primary">Fermer</button>
+                                                    <input type="submit" name="action" value="Connexion">
+                                                </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>';
             }
             ?>
               <!-- Bouton execution modal -->
