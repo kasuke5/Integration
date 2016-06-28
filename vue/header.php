ec7036147c84eec93cf09d4ee1f3512f6194e835
tjchris34 <thiburce@intechinfo.fr>
2016-06-28 14:47:02 +0200

    modif page ajout event

--- vue/header.php
+++ vue/header.php
@@ -139,34 +139,23 @@ jQuery(".window").hide();
               echo'<li><a href="/recherche">Recherche</a></li><li><a href="/tableau_bord">Mes évènements</a></li><li><a href="/deconnexion">Se déconnecter</a></li><li><a href="http://mail.eventizi.itinet.fr" target="_blank">Boîte Mail</a></li> ';
             }else{
               #echo'<li><a href="vue/modal.php">Connexion</a></li><li><a href="/inscription">Inscription</a></li></div>';
-              echo'<li><a href="#modal-2" data-toggle="modal">connexion</a></li>
-              <div class="modal" id="modal-2">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="login">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <div class="login-triangle"></div>
-                      <h2 class="login-header">Connexion</h2>
-                    </div>
-                    <div class="modal-body">
-                      <form class="form-horizontal" role="form" method="post">
-                            <input type="text" name="login" id="login" placeholder="Pseudo">
-                            <br />
-                            <input type="password" name="pass" id="pass" placeholder="Mot de passe">
-                            <br />
-                                                </div>
-                                            </div>
-                                                <div class="modal-footer">
-                            <button type="button" data-dismiss="modal" class="btn btn-primary">Fermer</button>
-                                                    <input type="submit" name="action" value="Connexion">
-                                                </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>';
+              echo'<div class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->';
             }
             ?>
               <!-- Bouton execution modal -->
