4479962b4ad3da67d765607d040a51f7f3603d7e
tjchris34 <thiburce@intechinfo.fr>
2016-06-29 11:34:11 +0200

    modif page ajout event

--- vue/ajout_evenement.php
+++ vue/ajout_evenement.php
@@ -12,9 +12,9 @@ include ('header.php');
 <script type="text/javascript" src="vue/js/chosen.jquery.min.js"></script>
 <script type="text/javascript" src="vue/js/fileinput.js"></script>
 <script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
-<script>
+<!--<script>
     webshims.setOptions('forms-ext', {types: 'date'});
-    webshims.polyfill('forms forms-ext');
+    webshims.polyfill('forms forms-ext');-->
 </script>
 <div class="event">
 <div class="login-triangle"></div>
