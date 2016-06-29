02b0156b37570659841e120188002acf0bdcb5c2
tjchris34 <thiburce@intechinfo.fr>
2016-06-29 19:28:09 +0200

    modif ajout event vue

--- vue/ajout_evenement.php
+++ vue/ajout_evenement.php
@@ -72,11 +72,10 @@ include ('header.php');
   <input  type="file" name="photo">
   <div id="bdd"></div>
    </div>
-   <div class="col-lg-2">
+   
   <label class="control-label">Description</label>      
    <textarea name="description" id="description" rows="4" cols="40"></textarea>
     <input type="submit" name="action" value="Créer">
-</div>
   </form>
 
   </div>
