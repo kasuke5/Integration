8539d92a0f03807fa9e6fc10361931ca267d59d5
Guillaume <gjauffret21@gmail.com>
2016-06-30 18:19:52 +0200

    maj ajout

--- controleur/ajout_evenement.php
+++ controleur/ajout_evenement.php
@@ -101,7 +101,7 @@ if(isset($_SESSION["id_user"])){
 							switch($site){
 								case 1:
 									$commande_web = "./script_web.sh 1 ".$name." 2 ".$name." ".$_SESSION["login_user"];
-									$commande_bdd = "./script_bdd.sh 1 ".$name;
+									$commande_bdd = "./script_bdd.sh 1 ".$name." ".$_SESSION["login_user"];
 									$commande_chat = "./script_orga.sh 1 ".$_SESSION["login_user"];
 									exec($commande_bdd);
 									break;
@@ -111,7 +111,7 @@ if(isset($_SESSION["id_user"])){
 									break;
 								case 3:
 									$commande_web = "./script_web.sh 1 ".$name." 1 ".$name." ".$_SESSION["login_user"];
-									$commande_bdd = "./script_bdd.sh 1 ".$name;
+									$commande_bdd = "./script_bdd.sh 1 ".$name" ".$_SESSION["login_user"];
 									$commande_chat = "./script_orga.sh 1 ".$_SESSION["login_user"];
 									exec($commande_bdd);
 									break;
