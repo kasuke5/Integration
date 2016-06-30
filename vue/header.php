77865dbffa497024e3a6f9532dd712d1b05a2f05
tjchris34 <thiburce@intechinfo.fr>
2016-06-30 14:38:14 +0200

    modif ajout event vue

--- vue/header.php
+++ vue/header.php
@@ -107,8 +107,8 @@ body {font:14px verdana, sans-serif;background:#000000;color:#C0C0C0;font-weight
               echo'<li><a href="/recherche">Recherche</a></li><li><a href="/tableau_bord">Mes évènements</a></li><li><a href="/deconnexion">Se déconnecter</a></li><li><a href="http://mail.eventizi.itinet.fr" target="_blank">Boîte Mail</a></li> ';
             }else{
               #echo'<li><a href="/connexion">Connexion</a></li><li><a href="/inscription">Inscription</a></li></div>';
-              echo '<li><a href="#" data-width="500" data-rel="popup_name" class="poplight">Connexion</a></li>
-              <div id="popup_name" class="popup_block">
+              echo '<li><a href="#" data-width="500" data-rel="popup1" class="poplight">Connexion</a></li>
+              <div id="popup1" class="popup_block">
               <div class="login">
   <div class="login-triangle"></div>
   <h2 class="login-header">Connexion</h2>
