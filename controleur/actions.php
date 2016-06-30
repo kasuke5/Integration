caa74c19220fd75c5b41817c42e2a998009c026c
Guillaume <gjauffret21@gmail.com>
2016-06-30 20:55:05 +0200

    modifs

--- controleur/actions.php
+++ controleur/actions.php
@@ -8,7 +8,7 @@
 	if($event["site"] ==1){
 		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 2 ".$event["event_title"].$_SESSION["login_user"];
 		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"];
-		//exec($commande_bdd);
+		exec($commande_bdd);
 	}elseif ($event["site"] ==2) {
 		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 1 ".$event["event_title"].$_SESSION["login_user"];
 	}elseif ($event["site"] ==3) {
@@ -16,7 +16,7 @@
 		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"];
 	}
 	include "controleur/tableau_bord.php";
-	//exec($commande_web);
+	exec($commande_web);
 }elseif($_POST["action"] == "inscrire"){
 	inscription_event($_SESSION["id_user"],$_POST["query"]);
 }elseif ($_POST["action"] =="desinscrire") {
