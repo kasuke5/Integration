134fe880958bce6e8fa7839422dea6f0452064f0
tjchris34 <thiburce@intechinfo.fr>
2016-06-30 14:04:16 +0200

    modif ajout event vue

--- vue/header.php
+++ vue/header.php
@@ -106,8 +106,8 @@ body {font:14px verdana, sans-serif;background:#000000;color:#C0C0C0;font-weight
               echo'<li><a href="/recherche">Recherche</a></li><li><a href="/tableau_bord">Mes évènements</a></li><li><a href="/deconnexion">Se déconnecter</a></li><li><a href="http://mail.eventizi.itinet.fr" target="_blank">Boîte Mail</a></li> ';
             }else{
               echo'<li><a href="/connexion">Connexion</a></li><li><a href="/inscription">Inscription</a></li></div>';
-             /* echo'
-              <li><a data-toggle="modal" data-target="#myModal">Connexion</a></li>
+              echo '<a href="#" data-width="500" data-rel="popup1" class="poplight">Connexion</a>';             
+              /*<li><a data-toggle="modal" data-target="#myModal">Connexion</a></li>
               <div class="modal" id="myModal" tabindex="-1" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -146,6 +146,47 @@ body {font:14px verdana, sans-serif;background:#000000;color:#C0C0C0;font-weight
 
       </div>
     </div>
+    <script type="text/javascript">
+      jQuery(function($){
+                     
+  //Lorsque vous cliquez sur un lien de la classe poplight
+  $('a.poplight').on('click', function() {
+    var popID = $(this).data('rel'); //Trouver la pop-up correspondante
+    var popWidth = $(this).data('width'); //Trouver la largeur
+
+    //Faire apparaitre la pop-up et ajouter le bouton de fermeture
+    $('#' + popID).fadeIn().css({ 'width': popWidth}).prepend('<a href="#" class="close"><img src="close_pop.png" class="btn_close" title="Close Window" alt="Close" /></a>');
+    
+    //Récupération du margin, qui permettra de centrer la fenêtre - on ajuste de 80px en conformité avec le CSS
+    var popMargTop = ($('#' + popID).height() + 80) / 2;
+    var popMargLeft = ($('#' + popID).width() + 80) / 2;
+    
+    //Apply Margin to Popup
+    $('#' + popID).css({ 
+      'margin-top' : -popMargTop,
+      'margin-left' : -popMargLeft
+    });
+    
+    //Apparition du fond - .css({'filter' : 'alpha(opacity=80)'}) pour corriger les bogues d'anciennes versions de IE
+    $('body').append('<div id="fade"></div>');
+    $('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn();
+    
+    return false;
+  });
+  
+  
+  //Close Popups and Fade Layer
+  $('body').on('click', 'a.close, #fade', function() { //Au clic sur le body...
+    $('#fade , .popup_block').fadeOut(function() {
+      $('#fade, a.close').remove();  
+  }); //...ils disparaissent ensemble
+    
+    return false;
+  });
+
+  
+});
+    </script>
 <!-- #Header Starts -->
 </body>
 
