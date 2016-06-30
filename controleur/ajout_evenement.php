21ac32b9f8e28d65b6bc2a9a8424f4ce26290072
tjchris34 <thiburce@intechinfo.fr>
2016-06-30 16:20:51 +0200

    modif ajout event vue

--- controleur/ajout_evenement.php
+++ controleur/ajout_evenement.php
@@ -97,20 +97,21 @@ if(isset($_SESSION["id_user"])){
 										$commande_mss = "./script_mss.sh 1 ".$_SESSION["login_user"];
 										exec($commande_mss); 
 									}
+									$name=url_transform($_POST["login_user"]);
 							switch($site){
 								case 1:
-									$commande_web = "./script_web.sh 1 ".$_POST["nom"]." 2 ".$_POST["nom"]." ".$_SESSION["login_user"];
-									$commande_bdd = "./script_bdd.sh 1 ".$_POST["nom"];
+									$commande_web = "./script_web.sh 1 ".$name." 2 ".$name." ".$_SESSION["login_user"];
+									$commande_bdd = "./script_bdd.sh 1 ".$name;
 									$commande_chat = "./script_orga.sh 1 ".$_SESSION["login_user"];
 									exec($commande_bdd);
 									break;
 								case 2:
-									$commande_web = "./script_web.sh 1 ".$_POST["nom"]." 1 ".$_POST["nom"]." ".$_SESSION["login_user"];
+									$commande_web = "./script_web.sh 1 ".$name." 1 ".$name." ".$_SESSION["login_user"];
 									$commande_chat = "./script_orga.sh 1 ".$_SESSION["login_user"];
 									break;
 								case 3:
-									$commande_web = "./script_web.sh 1 ".$_POST["nom"]." 1 ".$_POST["nom"]." ".$_SESSION["login_user"];
-									$commande_bdd = "./script_bdd.sh 1 ".$_POST["nom"];
+									$commande_web = "./script_web.sh 1 ".$name." 1 ".$name." ".$_SESSION["login_user"];
+									$commande_bdd = "./script_bdd.sh 1 ".$name;
 									$commande_chat = "./script_orga.sh 1 ".$_SESSION["login_user"];
 									exec($commande_bdd);
 									break;
