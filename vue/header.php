3274633cddaa03e666529d258269423c92d92a56
tjchris34 <thiburce@intechinfo.fr>
2016-06-30 15:15:16 +0200

    modif ajout event vue

--- vue/header.php
+++ vue/header.php
@@ -112,7 +112,7 @@ body {font:14px verdana, sans-serif;background:#000000;color:#C0C0C0;font-weight
               <div class="login">
   <div class="login-triangle"></div>
   <h2 class="login-header">Connexion</h2>
-  <form class="form-horizontal" role="form" method="post">
+  <form class="form-horizontal" action="connexion" role="form" method="post">
     <p><input type="text" name="login" id="login" placeholder="Pseudo"></p>
     <p><input type="password" name="pass" id="pass" placeholder="Mot de passe"></p>
     <p><input type="submit" name="action" value="Connexion"></p>
