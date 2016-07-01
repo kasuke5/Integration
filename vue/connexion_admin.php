cfa7bfed25a4fba9caf599bcc00d256f29fb75cd
tjchris34 <thiburce@intechinfo.fr>
2016-07-01 09:47:01 +0200

    modif page connexion admin vue

--- vue/connexion_admin.php
+++ vue/connexion_admin.php
@@ -3,7 +3,7 @@ include("header.php");
 $_SESSION['admin'];
 $admin;
 ?>
-<div class="login" style="margin:80px auto auto">
+<div class="login">
   <div class="login-triangle"></div>
   
   <h2 class="login-header">Connexion Admin</h2>
