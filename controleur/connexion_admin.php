a6d6863f76ec8f10225b5b3dfca637dab746119a
kasuke5 <ratchet_furtif@yahoo.fr>
2016-07-01 11:20:52 +0200

    admin mieux

--- controleur/connexion_admin.php
+++ controleur/connexion_admin.php
@@ -3,13 +3,15 @@ include('modele/connexion_admin.php');
 include_once('modele/connexion_bdd.php');
 
 if(isset($_POST['login_admin']) && isset($_POST['pass_admin'])) {
-	$admin = get_admin($_POST['login_admin']);
-	echo $admin;
-	if ($admin != NULL) {
-		$_SESSION['admin'] = 1;
-	}
-} elseif($_SESSION['admin'] = 1) {
-	include('controleur/admin.php');
+		$admin = get_admin($_POST['login_admin']);
+		if ($admin) {
+			$_SESSION['admin'] = 1;
+		} else {
+			include('vue/connexion_admin.php');
+		}
+} 
+if($_SESSION['admin'] == 1) {
+		include('controleur/admin.php');
 } else {
 	include('vue/connexion_admin.php');
 }
--- modele/connexion_admin.php
+++ modele/connexion_admin.php
@@ -1,7 +1,7 @@
 <?php 
 function get_admin($login) {
 	global $bdd;
-	$req = $bdd->prepare('SELECT * FROM t_user WHERE user_admin = 1 AND user_login = "?"');
+	$req = $bdd->prepare('SELECT * FROM t_user WHERE user_admin = 1 AND user_login = ?');
 	$req->execute(array($login)) or die ( print_r($req->errorInfo()) );
 	$donnees = $req->fetch();
 	return $donnees;
