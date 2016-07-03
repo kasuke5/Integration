5c143ff5c6bf5e570d710792ec003b24b7487d3c
Guillaume <gjauffret21@gmail.com>
2016-07-03 12:45:19 +0200

    modifs suppression

--- controleur/actions.php
+++ controleur/actions.php
@@ -5,15 +5,16 @@
 	if($_POST["action"] == "supprimer"){
 	$event = get_event_by_id($_POST["id"]);
 	remove_event($_POST["id"],$_SESSION["id_user"]);
+	$name = url_transform($event["event_title"];
 	if($event["site"] ==1){
-		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 2 ".$_SESSION["pass_user"]." ".$_SESSION["login_user"];
-		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"]." ".$_SESSION["pass_user"];
+		$commande_web = "./script_web.sh 2 ".$name." 2 ".$_SESSION["pass_user"]." ".$_SESSION["login_user"];
+		$commande_bdd = "./script_bdd.sh 2 ".$name." ".$_SESSION["login_user"]." ".$_SESSION["pass_user"];
 		exec($commande_bdd);
 	}elseif ($event["site"] ==2) {
-		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 1 ".$_SESSION["pass_user"]." ".$_SESSION["login_user"];
+		$commande_web = "./script_web.sh 2 ".$name." 1 ".$_SESSION["pass_user"]." ".$_SESSION["login_user"];
 	}elseif ($event["site"] ==3) {
-		$commande_web = "./script_web.sh 2 ".$event["event_title"]." 1 ".$_SESSION["pass_user"]." ".$_SESSION["login_user"];
-		$commande_bdd = "./script_bdd.sh 2 ".$event["event_title"]." ".$_SESSION["login_user"];
+		$commande_web = "./script_web.sh 2 ".$name." 1 ".$_SESSION["pass_user"]." ".$_SESSION["login_user"];
+		$commande_bdd = "./script_bdd.sh 2 ".$name." ".$_SESSION["login_user"];
 		exec($commande_bdd);
 	}
 	exec($commande_web);
