ed962f968ede121d30d4918b3c728bb99aeff017
tjchris34 <thiburce@intechinfo.fr>
2016-07-07 10:32:55 +0200

    modif script suppr

--- script_suppression.sh
+++ script_suppression.sh
@@ -3,11 +3,11 @@
 # $1 -> nom membre $2 -> mot de passe 
 source /var/www/eventizi.itinet.fr/scripts/source.sh
 
-sudo ./script_mail.sh 2 $1 
-sudo ./script_mss.sh 2 $1 
+sudo ./script_mail.sh 2 $2 
+sudo ./script_mss.sh 2 $2 
 
 mysql --user=$mysql_login --password=$mysql_pass << EOF
-DROP USER '$1'@'localhost';
+DROP USER '$2'@'localhost';
 EOF
 
 sudo rm -r /var/www/$2
